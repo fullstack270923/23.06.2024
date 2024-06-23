066e3ccbaa65ced826af32d5ed4a69ffa838e21f
fullstack290623 <fullstack290623@gmail.com>
2024-06-23 19:09:45 +0300

    .

---
+++ books_h2/src/main/java/com/example/demo/repository/BookRepository.java
@@ -0,0 +1,39 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Book;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+// DAL
+public interface BookRepository extends CrudRepository<Book, Integer> {
+
+    // knex.getbyid(1)
+    // knex.insert
+
+    /*
+    <S extends T> S save(S entity);
+
+    <S extends T> Iterable<S> saveAll(Iterable<S> entities);
+
+    Optional<T> findById(ID id);
+
+    boolean existsById(ID id);
+
+    Iterable<T> findAll();
+
+    Iterable<T> findAllById(Iterable<ID> ids);
+
+    long count();
+
+    void deleteById(ID id);
+
+    void delete(T entity);
+
+    void deleteAllById(Iterable<? extends ID> ids);
+
+    void deleteAll(Iterable<? extends T> entities);
+
+    void deleteAll();
+     */
+
+}
