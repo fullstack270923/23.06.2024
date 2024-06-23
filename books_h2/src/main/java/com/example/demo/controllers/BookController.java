379175bfef0fe82bdc269e11d0d6ea53bbe95b7a
fullstack290623 <fullstack290623@gmail.com>
2024-06-23 21:32:26 +0300

    Update BookController.java

--- books_h2/src/main/java/com/example/demo/controllers/BookController.java
+++ books_h2/src/main/java/com/example/demo/controllers/BookController.java
@@ -2,9 +2,15 @@ package com.example.demo.controllers;
 
 import com.example.demo.model.Book;
 import com.example.demo.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -12,10 +18,38 @@ import java.util.Optional;
 @RequestMapping("/api/books")
 public class BookController {
 
-    private final BookRepository bookRepository;
+    // 2. Autowired
+    //    @Autowired
+    //    private BookRepository bookRepository;
 
+    // 1. ctor gets the repo
+    private final BookRepository bookRepository;
     public BookController(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
+    }
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @GetMapping("/jdbc")
+    public Iterable<Book> getAllJdbc() {
+        String query = "SELECT * FROM BOOK";
+        List<Book> result = jdbcTemplate.query(query, new BookRowMapper());
+        return result;
+    }
+
+    private static class BookRowMapper implements RowMapper<Book>  {
+        @Override
+        // id  title pages author
+        //  1   james  128  bond
+        public Book mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Integer id = rs.getInt("id");
+            String title = rs.getString("title");
+            Integer pages = rs.getInt("pages");
+            String author = rs.getString("Author");
+            Book book = new Book(id, title, pages, author);
+            return book;
+        }
     }
 
     @GetMapping
